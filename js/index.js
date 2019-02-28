a35836af269649d2f7343bcab3bb87a9711dfd78
maanlamp <32102935+maanlamp@users.noreply.github.com>
2019-02-28 22:00:35 +0100

    Added request options, custom API.logError

--- js/index.js
+++ js/index.js
@@ -8,7 +8,7 @@ export class API {
 	constructor ({
 		CORSProxy = "https://cors-anywhere.herokuapp.com/",
 		baseURL = "https://zoeken.oba.nl/api/v1/",
-		key = "NULL"
+		key = "NO_KEY_PROVIDED"
 	} = {}) {
 		this._URL = CORSProxy
 			+ baseURL
@@ -17,18 +17,28 @@ export class API {
 			+ key;
 	}
 
+	static logError (error) {
+		console.error(
+			"%c%s\n%c%s",
+				"color: #F92672;",
+				error.message,
+				"color: black;",
+				error.stack.replace(/^.+\n/, ""));
+	}
+
 	_parsePartial (partial) {
-		//Expect: endpoint/query (can include spaces){count?,batchsize?}
-		const regex = /(?<endpoint>\w+)\/(?<value>[^\{]+)(?:\{(?<max>\d+)?(?:,\s*)?(?<batchsize>\d+)?\})?/;
+		//if (partial.toString() === "[object Object]") return {}; //If partial is omitted, allow for options fallback
+		//Expect: endpoint/query (can include spaces){count?,pagesize?}
+		const regex = /(?<endpoint>\w+)\/?(?<value>[^/\{]+)(?:\{(?<max>\d+)?(?:,\s*)?(?<pagesize>\d+)?\})?/;
 		if (!regex.test(partial)) throw new Error(`'${partial}' is not a valid endpoint and/or query.`);
 
 		const {
 			endpoint,
 			value,
 			max = 20,
-			batchsize = 20
+			pagesize = 20
 		} = partial.match(regex).groups;
-		if (batchsize > 20) console.warn(`API supports, at most, 20 results at a time – not ${batchsize}.`);
+		if (pagesize > 20) console.warn(`API supports, at most, 20 results at a time – not ${pagesize}.`);
 
 		const query = (() => {
 			switch (endpoint) {
@@ -43,23 +53,25 @@ export class API {
 			endpoint,
 			query: `&${query}=${value.trim()}`,
 			max: Number(max),
-			batchsize: Math.min(
+			pagesize: Math.min(
 				Number(max),
-				Number(batchsize))
+				Number(pagesize))
 		};
 	}
 
-	async _getRequestSpecifics (partial) {
+	async _getRequestSpecifics (partial, options) {
+		if (partial === undefined) throw new Error("Failed to get request specifics. Did you forget a url?");
+
 		const {
 			endpoint,
 			query,
 			max,
-			batchsize
-		} = this._parsePartial(partial);
+			pagesize
+		} = Object.assign({}, this._parsePartial(partial), options);
 		const url = this._URL.replace("ENDPOINT", endpoint) + query;
 		const {count, context} = await this._ping(url);
-		const batches = Math.ceil(Math.min(max, count) / batchsize);
-		const builtURL = url + `&pagesize=${batchsize}&refine=true`;
+		const batches = Math.ceil(Math.min(max, count) / pagesize);
+		const builtURL = url + `&pagesize=${pagesize}&refine=true`;
 
 		return {batches, builtURL, context, count};
 	}
@@ -76,13 +88,13 @@ export class API {
 			.catch(supressPingError);
 	}
 
-	async createStream (partial) {
+	async createStream (partial, options = {}) {
 		const {
 			batches,
 			builtURL,
 			context,
 			count
-		} = await this._getRequestSpecifics(partial);
+		} = await this._getRequestSpecifics(partial, options);
 
 		if (count === 0) throw new Error(`No results found for '${partial}'.`);
 
@@ -91,16 +103,16 @@ export class API {
 				.map(url => smartRequest(url)))
 			.pipe(XMLToJSON)
 			.pipe(cleanAquabrowserJSON)
-			.catch(console.error);
+			.catch(API.logError);
 	}
 
-	async createIterator (partial) {
+	async createIterator (partial, options = {}) {
 		const {
 			batches,
 			builtURL,
 			context,
 			count
-		} = await this._getRequestSpecifics(partial);
+		} = await this._getRequestSpecifics(partial, options);
 
 		if (count === 0) throw new Error(`No results found for '${partial}'.`);
 
@@ -113,20 +125,20 @@ export class API {
 				yield await smartRequest(url)
 					.then(XMLToJSON)
 					.then(cleanAquabrowserJSON)
-					.catch(console.error);
+					.catch(API.logError);
 			}
 		}
 
 		return iterator();
 	}
 
-	async createPromise (partial) {
+	async createPromise (partial, options = {}) {
 		const {
 			batches,
 			builtURL,
 			context,
 			count
-		} = await this._getRequestSpecifics(partial);
+		} = await this._getRequestSpecifics(partial, options);
 
 		if (count === 0) throw new Error(`No results found for '${partial}'.`);
 
@@ -135,7 +147,7 @@ export class API {
 			.map(url => smartRequest(url)
 				.then(XMLToJSON)
 				.then(cleanAquabrowserJSON)
-				.catch(console.error));
+				.catch(API.logError));
 	}
 }
 
