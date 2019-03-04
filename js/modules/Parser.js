318a67e70eb7dda2a80d7b888cfcbfe1791dc88a
maanlamp <32102935+maanlamp@users.noreply.github.com>
2019-03-04 15:09:50 +0100

    Update Parser.js

--- js/modules/Parser.js
+++ js/modules/Parser.js
@@ -65,46 +65,47 @@ export function cleanAquabrowserJSON (json) {
 	const root = json.aquabrowser.results.result;
 
 	//What have I done...
-	function clean (item) {
-		const [lastname, firstname] = (item.authors||{"main-author":{_text:"defined, un"}})["main-author"]._text.split(", ");
-		let [raw, pages, type, size] = /\[?(\d+)\]? (?:ongenummerde)? ?p(?:agina(?:'|&#x2019;)s)?[^;]*?:? ?([^;]+)? ; (\d+(?:x| ?× ?)?\d* cm)/g.exec((item.description||{"physical-description":{}})["physical-description"]._text) || [null, null, null, null];
-		if (!size) size = /.*(\d+(?:x| × )?\d* cm)/g.exec((item.description||{"physical-description":{}})["physical-description"]._text);
-		return {
-			author: {
-				fullname: `${firstname} ${lastname}`,
-				firstname: firstname,
-				lastname: lastname
-			},
-			images: [item.coverimages.coverimage].flat().map(coverimage => coverimage._text).filter(url => !url.includes("~")),
-			title: {
-				// short: (item.titles||{"short-title":{}})["short-title"]._text, //For some reason, this ALWAYS errors.. I don't understand :(
-				full: item.titles.title._text
-			},
-			format: item.formats.format._text,
-			identifiers: Object.entries(item.identifiers||{}).map(([identifier, body]) => {return {[identifier]: body._text}}),
-			publication: {
-				year: (item.publication||{year:{}}).year._text,
-				publisher: (item.publication||{publishers:{publisher:{}}}).publishers.publisher._text,
-				place: (item.publication||{publishers:{publisher:{place:undefined}}}).publishers.publisher.place
-			},
-			languages: {
-				this: (item.languages||{language:{}}.language)._text,
-				original: ((item.languages||{})["original-language"] || (item.languages||{}).language || {})._text
-			},
-			subjects: [(item.subjects||{})["topical-subject"]||{}].flat().map(subject => subject._text),
-			genres: [(item.genres||{genre:{}}).genre].flat().map(genre => genre._text),
-			characteristics: {
-				pages: Number(pages),
-				size: size,
-				types: (type||"").split(",").map(string => string.trim()),
-				raw: raw
-			},
-			summary: (item.summaries||{summary:{}}).summary._text,
-			notes: [(item.notes || {}).note||{}].flat().map(note => note._text || null).filter(note => note !== null),
-			targetAudiences: [(item["target-audiences"] && item["target-audiences"] || {})["target-audience"]||{}].flat().map(audience => audience._text || null).filter(audience => audience !== null),
-			series: ((item.series && item.series["series-title"] && item.series["series-title"]._text) || null)
-		};
-	}
+	// function clean (item) {
+	// 	const [lastname, firstname] = (item.authors||{"main-author":{_text:"defined, un"}})["main-author"]._text.split(", ");
+	// 	let [raw, pages, type, size] = /\[?(\d+)\]? (?:ongenummerde)? ?p(?:agina(?:'|&#x2019;)s)?[^;]*?:? ?([^;]+)? ; (\d+(?:x| ?× ?)?\d* cm)/g.exec((item.description||{"physical-description":{}})["physical-description"]._text) || [null, null, null, null];
+	// 	if (!size) size = /.*(\d+(?:x| × )?\d* cm)/g.exec((item.description||{"physical-description":{}})["physical-description"]._text);
+	// 	return {
+	// 		author: {
+	// 			fullname: `${firstname} ${lastname}`,
+	// 			firstname: firstname,
+	// 			lastname: lastname
+	// 		},
+	// 		images: [item.coverimages.coverimage].flat().map(coverimage => coverimage._text).filter(url => !url.includes("~")),
+	// 		title: {
+	// 			// short: (item.titles||{"short-title":{}})["short-title"]._text, //For some reason, this ALWAYS errors.. I don't understand :(
+	// 			full: item.titles.title._text
+	// 		},
+	// 		format: item.formats.format._text,
+	// 		identifiers: Object.entries(item.identifiers||{}).map(([identifier, body]) => {return {[identifier]: body._text}}),
+	// 		publication: {
+	// 			year: (item.publication||{year:{}}).year._text,
+	// 			publisher: (item.publication||{publishers:{publisher:{}}}).publishers.publisher._text,
+	// 			place: (item.publication||{publishers:{publisher:{place:undefined}}}).publishers.publisher.place
+	// 		},
+	// 		languages: {
+	// 			this: (item.languages||{language:{}}.language)._text,
+	// 			original: ((item.languages||{})["original-language"] || (item.languages||{}).language || {})._text
+	// 		},
+	// 		subjects: [(item.subjects||{})["topical-subject"]||{}].flat().map(subject => subject._text),
+	// 		genres: [(item.genres||{genre:{}}).genre].flat().map(genre => genre._text),
+	// 		characteristics: {
+	// 			pages: Number(pages),
+	// 			size: size,
+	// 			types: (type||"").split(",").map(string => string.trim()),
+	// 			raw: raw
+	// 		},
+	// 		summary: (item.summaries||{summary:{}}).summary._text,
+	// 		notes: [(item.notes || {}).note||{}].flat().map(note => note._text || null).filter(note => note !== null),
+	// 		targetAudiences: [(item["target-audiences"] && item["target-audiences"] || {})["target-audience"]||{}].flat().map(audience => audience._text || null).filter(audience => audience !== null),
+	// 		series: ((item.series && item.series["series-title"] && item.series["series-title"]._text) || null)
+	// 	};
+	// }
 
-	return root.map(clean);
+	// return root.map(clean);
+	return item;
 }
