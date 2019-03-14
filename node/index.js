c6450e563cdf80b196bb85508bca31ede851d362
maanlamp <32102935+maanlamp@users.noreply.github.com>
2019-03-14 14:28:42 +0100

    Added Node.js support

---
+++ node/index.js
@@ -0,0 +1,252 @@
+const PromiseStream = require("promisestream");
+const smartfetch = require("smartfetch");
+
+const smartfetchOptions = {
+	store: {
+		values: new Map(),
+		get: (key) => this.values.get(key),
+		set: (key, value) => this.values.set(key, value)
+	}
+};
+
+function range (size = 0, end = size) {
+	//refactor this shizzle?
+	if (size === end) return [...Array(size).keys()];
+	return [...Array(end - size).keys()].map((_, i) => i + size);
+}
+
+class Pong {
+	constructor (count = 0, context = "") {
+		this.count = Number(count);
+		this.context = String(context);
+	}
+}
+
+function detectPingError (res) {
+	if (!res.ok) throw new Error(`Cannot ping ${res.url} ${res.status} (${res.statusText})`);
+	return res;
+}
+
+function buildPong (json) {
+	return new Pong(
+		Number(json.aquabrowser.meta.count._text),
+		String(json.aquabrowser.meta.rctx._text));
+}
+
+function supressPingError (err) {
+	console.warn(`Supressed ${err}`);
+	return new Pong();
+}
+
+function XMLToJSON (xml) {
+	const parser = new DOMParser();
+	const doc = parser.parseFromString(xml, "application/xml");
+
+	function parse (xmldoc) {
+		const nodeTypes = {
+			"element": 1,
+			"text": 3
+		};
+		let json = {};
+
+		if (xmldoc.nodeType === nodeTypes["element"]) {
+			if (xmldoc.attributes.length > 0) {
+				json["_attributes"] = {};
+			}
+
+			for (const attr of xmldoc.attributes) {
+				json["_attributes"][attr.nodeName] = attr.nodeValue;
+			}
+		} else if (xmldoc.nodeType === nodeTypes["text"]) {
+			json = xmldoc.nodeValue;
+		}
+
+		if (xmldoc.hasChildNodes()) {
+			for (const child of xmldoc.childNodes) {
+				const nodeName = child.nodeName.replace(/^#/, "_");
+
+				if (json[nodeName] === undefined) {
+					json[nodeName] = parse(child);
+				} else {
+					if (json[nodeName].push === undefined) {
+						json[nodeName] = Array(json[nodeName]);
+					}
+					json[nodeName].push(parse(child));
+				}
+			}
+		}
+
+		return json;
+	}
+
+	return parse(doc);
+}
+
+function handleJSONParserError (json) {
+	const err = json.aquabrowser.error;
+	if (err) throw new Error(`${err.code._text} ${err.reason._text}`);
+	return json;
+}
+
+module.exports = class API {
+	constructor ({
+		CORSProxy = "https://cors-anywhere.herokuapp.com/",
+		baseURL = "https://zoeken.oba.nl/api/v1/",
+		key = "NO_KEY_PROVIDED"
+	} = {}) {
+		this._context = null;
+		this._URL = CORSProxy
+			+ baseURL
+			+ "ENDPOINT" //will be `.replace`d later (is this a good practise?)
+			+ "?authorization="
+			+ key;
+	}
+
+	static logError (error) {
+		console.error(
+			"%c%s\n%c%s",
+				"color: #F92672;",
+				error.message,
+				"color: black;",
+				error.stack.replace(/^.+\n/, ""));
+
+		throw error;
+	}
+
+	_parsePartial (partial) {
+		//if (partial.toString() === "[object Object]") return {}; //If partial is omitted, allow for options fallback
+		//Expect: endpoint/query (can include spaces){count?,pagesize?}
+		const regex = /(?<endpoint>\w+)\/?(?<value>[^/\{]+)(?:\{(?<max>\d+)?(?:,\s*)?(?<pagesize>\d+)?\})?/;
+		if (!regex.test(partial)) throw new Error(`'${partial}' is not a valid endpoint and/or query.`);
+
+		const {
+			endpoint,
+			value,
+			max = 20,
+			pagesize = 20
+		} = partial.match(regex).groups;
+		if (pagesize > 20) console.warn(`API supports, at most, 20 results at a time – not ${pagesize}.`);
+
+		const query = (() => {
+			switch (endpoint) {
+				case "search": return "q";
+				case "details": //fallthrough
+				case "availability": return "id";
+				default: throw new Error(`Unknown/unsupported endpoint '${endpoint}'`);
+			}
+		})();
+
+		return {
+			endpoint,
+			query: encodeURI(`&${query}=${value.trim()}`),
+			max: Number(max),
+			pagesize: Math.min(
+				Number(max),
+				Number(pagesize))
+		};
+	}
+
+	async _getRequestSpecifics (partial, options) {
+		if (partial === undefined) throw new Error("Failed to get request specifics. Did you forget a url?");
+
+		const {
+			endpoint,
+			query,
+			max,
+			pagesize
+		} = Object.assign({}, this._parsePartial(partial), options);
+		const url = this._URL.replace("ENDPOINT", endpoint) + query;
+		const {count, context} = await this._ping(url, this._context);
+		const batches = Math.ceil(Math.min(max, count) / pagesize);
+		const builtURL = url + `&pagesize=${pagesize}&refine=true`;
+
+		this._context = context;
+
+		return {batches, builtURL, count};
+	}
+
+	_ping (url, context) {
+		const builtURL = (context !== null)
+			? url + `&pagesize=1&refine=false&rctx=${context}`
+			: url + `&pagesize=1&refine=false`;
+
+		return fetch(builtURL) //test if it's beneficial to use smartrequest here
+			.then(detectPingError)
+			.then(res => res.text())
+			.then(XMLToJSON)
+			.then(handleJSONParserError)
+			.then(buildPong)
+			.catch(supressPingError);
+	}
+
+	async createStream (partial, options = {}) {
+		const {
+			batches,
+			builtURL,
+			count
+		} = await this._getRequestSpecifics(partial, options);
+
+		if (count === 0) throw new Error(`No results found for '${partial}'.`);
+
+		return new PromiseStream(
+				range(batches)
+					.map(index => builtURL + `&page=${index + 1}&rctx=${this._context}`)
+					.map(url => smartfetch(url, smartfetchOptions)))
+			.pipe(XMLToJSON)
+			.catch(API.logError);
+	}
+
+	async createIterator (partial, options = {}) {
+		const {
+			batches,
+			builtURL,
+			count
+		} = await this._getRequestSpecifics(partial, options);
+
+		if (count === 0) throw new Error(`No results found for '${partial}'.`);
+
+		async function* iterator () {
+			const requests = range(batches)
+				.map(index => builtURL + `&page=${index + 1}&rctx=${this._context}`);
+
+			while (requests.length > 0) {
+				const url = requests.shift();
+				yield await smartfetch(url, smartfetchOptions)
+					.then(XMLToJSON)
+					.catch(API.logError);
+			}
+		}
+
+		return iterator.call(this);
+	}
+
+	async createPromise (partial, options = {}) {
+		const {
+			batches,
+			builtURL,
+			count
+		} = await this._getRequestSpecifics(partial, options);
+
+		if (count === 0) throw new Error(`No results found for '${partial}'.`);
+
+		return range(batches)
+			.map(index => builtURL + `&page=${index + 1}&rctx=${this._context}`)
+			.map(url => smartfetch(url, smartfetchOptions)
+				.then(XMLToJSON)
+				.catch(API.logError));
+	}
+
+	availability (frabl) { //I hate this "solution", please fix dis kty <3
+		const url = this._URL.replace("ENDPOINT", "availability");
+		return smartfetch(url + `&frabl=${frabl}`, smartfetchOptions)
+			.then(XMLToJSON)
+			.catch(API.logError);
+	}
+
+	details (frabl) { //I hate this "solution", please fix dis kty <3
+		const url = this._URL.replace("ENDPOINT", "details");
+		return smartfetch(url + `&frabl=${frabl}`, smartfetchOptions)
+			.then(XMLToJSON)
+			.catch(API.logError);
+	}
+}
